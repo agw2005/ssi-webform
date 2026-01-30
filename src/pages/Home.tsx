2950677a228b82e367fb76fc46d245ca3f8f7423
agw2005 <danial.agw2005@gmail.com>
2026-01-30 15:52:57 +0700

    feat(Home.tsx): added section filter element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,49 @@
 import Primitive from "../components/primitive";
 
+const SECTIONS = [
+  "MIS",
+  "GA & Personnel",
+  "Accounting",
+  "Purchasing",
+  "PSC",
+  "BM (HRDC)",
+  "RnD",
+  "EC QA-QC",
+  "EXIM",
+  "Material-Control",
+  "FG WHSE",
+  "EC Equipment Engineering OPTO",
+  "EC Production OPTO",
+  "EC Process Engineering OPTO",
+  "FCS",
+  "Process Control",
+  "Job Innovation",
+  "Product Innovation",
+  "Management",
+  "EC Equipment Engineering Compound",
+  "EC Process Engineering Compound",
+  "EC Production Compound",
+];
+
 const Home = () => {
   return (
     <Primitive>
-      <div>Hello World</div>
+      <div className="flex gap-5">
+        <select
+          name="filter-section"
+          id="filter-section"
+          className="py-2 px-4 h-10 rounded-xl border max-w-48"
+        >
+          <option value="All Section">All Section</option>
+          {SECTIONS.map((section, index) => {
+            return (
+              <option key={index} value={section}>
+                {section}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </Primitive>
   );
 };
