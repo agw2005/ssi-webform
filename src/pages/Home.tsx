e14cf91658fdb80bdb4c0fc5854b2c8012715a1c
agw2005 <danial.agw2005@gmail.com>
2026-01-30 15:56:23 +0700

    feat(Home.tsx): added search input element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -75,6 +75,21 @@ const Home = () => {
             );
           })}
         </select>
+        <div className="flex">
+          <input
+            type="search"
+            id="search"
+            name="search"
+            placeholder="Search"
+            className="border rounded-l-4xl h-10 py-2 px-4 outline-none"
+          />
+          <button
+            type="button"
+            className="py-2 px-4 h-10 border rounded-r-4xl hover:text-white hover:bg-black hover:border-black"
+          >
+            Search
+          </button>
+        </div>
       </div>
     </Primitive>
   );
