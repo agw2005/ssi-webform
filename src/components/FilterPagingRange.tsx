67a98092b1c157e1ae94de34b4005ff8ae913406
agw2005 <danial.agw2005@gmail.com>
2026-01-31 12:27:26 +0700

    feat(FilterPagingRange.tsx): added usestate to change the count

--- src/components/FilterPagingRange.tsx
+++ src/components/FilterPagingRange.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const FilterPagingRange = () => {
+  const [pagingLimit, setPagingLimit] = useState(1);
+
   return (
     <div className="flex">
       <div className="border rounded-l-xl h-10 py-2 px-4 bg-black text-white border-black text-center select-none">
@@ -9,8 +13,10 @@ const FilterPagingRange = () => {
         id="items-quantity"
         name="items-quantity"
         min={1}
-        value={1}
-        step={1}
+        value={pagingLimit}
+        onChange={(newLimit) =>
+          setPagingLimit(Number(newLimit.currentTarget.value))
+        }
         className="rounded-r-xl py-2 px-4 h-10 border outline-none max-w-24 text-center"
       />
     </div>
