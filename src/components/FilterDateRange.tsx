f09f322c30f67b5c4415459da898d1968deff106
agw2005 <danial.agw2005@gmail.com>
2026-02-01 11:51:12 +0700

    feat(FilterDateRange.tsx): added select none to the date filter non-input texts

--- src/components/FilterDateRange.tsx
+++ src/components/FilterDateRange.tsx
@@ -1,7 +1,7 @@
 const FilterDateRange = () => {
   return (
     <div className="flex">
-      <div className="border rounded-l-xl h-10 py-2 px-4 bg-black text-white border-black text-center">
+      <div className="border rounded-l-xl h-10 py-2 px-4 bg-black text-white border-black text-center select-none">
         Date from
       </div>
       <input
@@ -10,14 +10,14 @@ const FilterDateRange = () => {
         name="datefrom"
         className="py-2 px-4 h-10 border outline-none"
       />
-      <div className="border h-10 py-2 px-4 bg-black text-white border-black text-center">
+      <div className="border h-10 py-2 px-4 bg-black text-white border-black text-center select-none">
         to
       </div>
       <input
         type="date"
         id="dateto"
         name="dateto"
-        className="rounded-r-xl py-2 px-4 h-10 border outline-none"
+        className="rounded-r-xl py-2 px-4 h-10 border outline-none select-none"
       />
     </div>
   );
