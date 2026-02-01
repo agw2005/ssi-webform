3639ab07458ba448b6f58d5ce9b5bcdcfe2ba2d1
agw2005 <danial.agw2005@gmail.com>
2026-02-01 13:11:41 +0700

    feat(Primitive.tsx): added tailwind responsive prefix

--- src/components/Primitive.tsx
+++ src/components/Primitive.tsx
@@ -16,24 +16,30 @@ const NAVIGATIONS = [
 const Primitive = ({ children }: PrimitiveProps) => {
   return (
     <div className="bg-yellow-600/25 min-h-screen pb-16">
-      <nav className="flex justify-between bg-black text-white pl-8 sticky inset-0">
-        <div className="flex gap-10 my-4">
+      <nav className="pl-2 lg:pl-4 xl:pl-6 2xl:pl-8 | flex justify-between gap-2 bg-black text-white sticky inset-0">
+        <div className="gap-4 lg:gap-6 xl:gap-8 2xl:gap-10 | my-1 lg:my-2 xl:my-3 2xl:my-4 | flex flex-wrap items-center text-center ">
           {NAVIGATIONS.map((navigation, index) => {
             return (
-              <a href={navigation.link} key={index}>
+              <a
+                href={navigation.link}
+                key={index}
+                className="text-xs lg:text-sm xl:text-base"
+              >
                 {navigation.name}
               </a>
             );
           })}
         </div>
-        <div className="flex">
+        <div className="flex flex-wrap">
           <ForexInformation />
-          <div className="px-8 hover:bg-white hover:text-black active:bg-gray-800 active:text-white flex items-center">
+          <div className="px-2 lg:px-4 xl:px-6 2xl:px-8 | bg-black hover:bg-white hover:text-black active:bg-gray-800 active:text-white | flex items-center">
             <p className="select-none">User</p>
           </div>
         </div>
       </nav>
-      <main className="mx-16 mt-16 bg-white p-4">{children}</main>
+      <main className="mx-4 lg:mx-8 xl:mx-12 2xl:mx-16 | mt-4 lg:mt-8 xl:mt-12 2xl:mt-16 | bg-white p-4">
+        {children}
+      </main>
     </div>
   );
 };
