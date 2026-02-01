3ed038ae5f4efa57d49b1ea23e629e13c4094a20
agw2005 <danial.agw2005@gmail.com>
2026-02-01 13:52:21 +0700

    feat(Search.tsx): added tailwind responsive prefix

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,11 +6,11 @@ const Search = () => {
         id="search"
         name="search"
         placeholder="Search"
-        className="border rounded-l-4xl h-10 py-2 px-4 outline-none"
+        className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | px-2 lg:px-3 xl:px-4 | border rounded-l-4xl py-2 outline-none"
       />
       <button
         type="button"
-        className="py-2 px-4 h-10 border rounded-r-4xl hover:text-white hover:bg-black hover:border-black"
+        className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | px-2 lg:px-3 xl:px-4 | py-2 border rounded-r-4xl hover:text-white hover:bg-black hover:border-black"
       >
         Search
       </button>
