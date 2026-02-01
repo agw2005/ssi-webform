a55bf899cf998140342136e9f58ab18d7e77d0a0
agw2005 <danial.agw2005@gmail.com>
2026-02-01 13:34:03 +0700

    feat(FilterDateRange.tsx): added tailwind responsive prefix

--- src/components/FilterDateRange.tsx
+++ src/components/FilterDateRange.tsx
@@ -1,23 +1,23 @@
 const FilterDateRange = () => {
   return (
     <div className="flex">
-      <div className="border rounded-l-xl h-10 py-2 px-4 bg-black text-white border-black text-center select-none">
+      <div className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | border rounded-l-xl py-2 px-4 bg-black text-white border-black text-center select-none">
         Date from
       </div>
       <input
         type="date"
         id="datefrom"
         name="datefrom"
-        className="py-2 px-4 h-10 border outline-none"
+        className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | py-2 px-4 border outline-none"
       />
-      <div className="border h-10 py-2 px-4 bg-black text-white border-black text-center select-none">
+      <div className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | border py-2 px-4 bg-black text-white border-black text-center select-none">
         to
       </div>
       <input
         type="date"
         id="dateto"
         name="dateto"
-        className="rounded-r-xl py-2 px-4 h-10 border outline-none select-none"
+        className="text-xs lg:text-sm xl:text-base | h-8 lg:h-9 xl:h-10 | rounded-r-xl py-2 px-4 border outline-none select-none"
       />
     </div>
   );
