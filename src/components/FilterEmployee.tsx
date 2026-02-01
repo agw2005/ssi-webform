4980a966798885fe5320ee3bca539b50ccd94f81
agw2005 <danial.agw2005@gmail.com>
2026-02-01 13:38:05 +0700

    fix(FilterEmployee.tsx): added missing height responsive prefix

--- src/components/FilterEmployee.tsx
+++ src/components/FilterEmployee.tsx
@@ -5,7 +5,7 @@ const FilterEmployee = () => {
     <select
       name="filter-in-charge"
       id="filter-in-charge"
-      className="text-xs lg:text-sm xl:text-base | max-w-36 lg:max-w-42 xl:max-w-48 | py-2 px-4 h-10 rounded-xl border"
+      className="text-xs lg:text-sm xl:text-base | max-w-36 lg:max-w-42 xl:max-w-48 | h-8 lg:h-9 xl:h-10 | py-2 px-4 rounded-xl border"
     >
       <option value="All Supervisor">All Supervisor</option>
       {EMPLOYEES.map((employee, index) => {
