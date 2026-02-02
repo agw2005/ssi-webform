4e2ef116c6941c3cfd9299ea16617456d80878a0
agw2005 <danial.agw2005@gmail.com>
2026-02-02 11:16:24 +0700

    feat(Budget.tsx): scaffold simple file resource dropdown

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -1,7 +1,29 @@
 import Primitive from "../components/Primitive";
 
+const OPTIONS = ["EXIM", "FCS", "GA", "MC", "MIS"];
+
 const Budget = () => {
-  return <Primitive>Hello World</Primitive>;
+  return (
+    <Primitive>
+      <div className="h-8 lg:h-9 xl:h-10 | flex gap-3 items-center">
+        <label>File Resource</label>
+        <select
+          name="file-resource"
+          id="file-resource"
+          className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-xl border"
+        >
+          <option value="Show All">Show All</option>
+          {OPTIONS.map((option, index) => {
+            return (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </Primitive>
+  );
 };
 
 export default Budget;
