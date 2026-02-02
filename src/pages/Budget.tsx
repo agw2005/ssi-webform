07b1d6cbc2d15a3bc704e15f28c4d0b83d58e174
agw2005 <danial.agw2005@gmail.com>
2026-02-02 11:26:08 +0700

    feat(Budget.tsx): scaffold simple period dropdown

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -1,26 +1,59 @@
 import Primitive from "../components/Primitive";
 
 const OPTIONS = ["EXIM", "FCS", "GA", "MC", "MIS"];
+const PERIODS = [
+  "2025LH",
+  "2025FH",
+  "2024LH",
+  "2024FH",
+  "2023LH",
+  "2023FH",
+  "2022LH",
+  "2022FH",
+  "2021LH",
+  "2021FH",
+  "2020LH",
+  "2020FH",
+];
 
 const Budget = () => {
   return (
     <Primitive>
-      <div className="h-8 lg:h-9 xl:h-10 | flex gap-3 items-center">
-        <label>File Resource</label>
-        <select
-          name="file-resource"
-          id="file-resource"
-          className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-xl border"
-        >
-          <option value="Show All">Show All</option>
-          {OPTIONS.map((option, index) => {
-            return (
-              <option key={index} value={option}>
-                {option}
-              </option>
-            );
-          })}
-        </select>
+      <div className="flex flex-col gap-2 w-max items-end">
+        <div className="h-8 lg:h-9 xl:h-10 | flex gap-3 items-center w-max">
+          <label>File Resource</label>
+          <select
+            name="file-resource"
+            id="file-resource"
+            className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-xl border"
+          >
+            <option value="Show All">Show All</option>
+            {OPTIONS.map((option, index) => {
+              return (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="h-8 lg:h-9 xl:h-10 | flex gap-3 items-center w-max">
+          <label>Period</label>
+          <select
+            name="file-resource"
+            id="file-resource"
+            className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-xl border"
+          >
+            <option value="Show All">Show All</option>
+            {PERIODS.map((option, index) => {
+              return (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
     </Primitive>
   );
