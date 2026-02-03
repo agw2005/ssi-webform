34b379ceda1a3874e03871ca9d42bad3e65d906d
agw2005 <danial.agw2005@gmail.com>
2026-02-03 10:27:29 +0700

    feat(index.ts): added simple test to connect to database

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import mysql from "mysql2/promise";
 
-const app = express();
+const server = express();
+const connection = await mysql.createConnection({
+  host: "localhost",
+  port: 3333,
+  user: "root",
+  password: "u101",
+  database: "dev",
+});
 
-app.get("/", (_, res) => {
+const testConnection = async () => {
+  try {
+    const [results, metadata] = await connection.query("SELECT * FROM test");
+    console.log(results);
+    console.log(metadata);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+server.get("/", (_, res) => {
   res.send("Hello World");
 });
 
-app.listen(3067, () => {
+server.listen(3067, () => {
   console.log("Server is running on http://localhost:3067");
+  testConnection();
 });
