4e006a5904e0717d8a2ec2ff746a09d6b377c9a6
agw2005 <danial.agw2005@gmail.com>
2026-02-04 09:18:34 +0700

    feat(Budget.tsx): made content of table never overflow

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -118,15 +118,15 @@ const Budget = () => {
         </div>
       </div>
 
-      <div className="overflow-x-auto h-163 mt-4">
-        <table className="table-auto border-collapse min-w-full max-w-full mt-4">
+      <div className="overflow-x-auto h-160 mt-4">
+        <table className="table-auto border-collapse mt-4">
           <thead className="sticky top-0 z-10 border">
             <tr>
               {COLUMNS.map((column, index) => {
                 return (
                   <th
                     key={index}
-                    className="text-xs lg:text-sm xl:text-base | min-w-32 border p-2 bg-blue-800 text-white border-black"
+                    className="text-xs lg:text-sm xl:text-base | border p-2 bg-blue-800 text-white border-black whitespace-nowrap text-center"
                   >
                     {column}
                   </th>
@@ -142,7 +142,7 @@ const Budget = () => {
                     return (
                       <td
                         key={index}
-                        className="text-xs lg:text-sm xl:text-base | border break-all p-2"
+                        className="text-xs lg:text-sm xl:text-base | border p-2 whitespace-nowrap text-center"
                       >
                         {placeholder[column as keyof typeof placeholder]}
                       </td>
