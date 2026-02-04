9183c8b3c48080d23017035d417141fadab40268
agw2005 <danial.agw2005@gmail.com>
2026-02-04 09:37:11 +0700

    feat(Budget.tsx): show budget content when view mode is on budget

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -73,87 +73,97 @@ const Budget = () => {
             View Budget
           </div>
         </div>
-        <div className="h-8 lg:h-9 xl:h-10 | flex items-center w-max">
-          <div className="text-xs lg:text-sm xl:text-base | rounded-l-xl h-full justify-self-center border flex items-center px-2 border-r-0 border-black bg-black text-white select-none">
-            File Resource
-          </div>
-          <select
-            name="file-resource"
-            id="file-resource"
-            className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-r-xl border outline-none"
-          >
-            <option value="Show All">Show All</option>
-            {OPTIONS.map((option, index) => {
-              return (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="h-8 lg:h-9 xl:h-10 | flex items-center w-max">
-          <div className="text-xs lg:text-sm xl:text-base | rounded-l-xl h-full justify-self-center border flex items-center px-2 border-r-0 border-black bg-black text-white select-none">
-            Period
-          </div>
-          <select
-            name="file-resource"
-            id="file-resource"
-            className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-r-xl border outline-none"
-          >
-            <option value="Show All">Show All</option>
-            {PERIODS.map((option, index) => {
-              return (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="h-8 lg:h-9 xl:h-10 | text-black hover:text-white active:text-black | bg-white hover:bg-black active:bg-white | border-black hover:border-blue-900 active:border-red-900 | flex items-center border px-4 rounded-xl">
-          <p className="text-xs lg:text-sm xl:text-base | select-none">
-            Search
-          </p>
-        </div>
+        {viewMode === "Budget" ? (
+          <>
+            <div className="h-8 lg:h-9 xl:h-10 | flex items-center w-max">
+              <div className="text-xs lg:text-sm xl:text-base | rounded-l-xl h-full justify-self-center border flex items-center px-2 border-r-0 border-black bg-black text-white select-none">
+                File Resource
+              </div>
+              <select
+                name="file-resource"
+                id="file-resource"
+                className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-r-xl border outline-none"
+              >
+                <option value="Show All">Show All</option>
+                {OPTIONS.map((option, index) => {
+                  return (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="h-8 lg:h-9 xl:h-10 | flex items-center w-max">
+              <div className="text-xs lg:text-sm xl:text-base | rounded-l-xl h-full justify-self-center border flex items-center px-2 border-r-0 border-black bg-black text-white select-none">
+                Period
+              </div>
+              <select
+                name="file-resource"
+                id="file-resource"
+                className="text-xs lg:text-sm xl:text-base | h-full px-4 rounded-r-xl border outline-none"
+              >
+                <option value="Show All">Show All</option>
+                {PERIODS.map((option, index) => {
+                  return (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="h-8 lg:h-9 xl:h-10 | text-black hover:text-white active:text-black | bg-white hover:bg-black active:bg-white | border-black hover:border-blue-900 active:border-red-900 | flex items-center border px-4 rounded-xl">
+              <p className="text-xs lg:text-sm xl:text-base | select-none">
+                Search
+              </p>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
 
-      <div className="overflow-x-auto h-160 mt-4">
-        <table className="table-auto border-collapse mt-4">
-          <thead className="sticky top-0 z-10 border">
-            <tr>
-              {COLUMNS.map((column, index) => {
+      {viewMode === "Budget" ? (
+        <div className="overflow-x-auto h-160 mt-4">
+          <table className="table-auto border-collapse mt-4">
+            <thead className="sticky top-0 z-10 border">
+              <tr>
+                {COLUMNS.map((column, index) => {
+                  return (
+                    <th
+                      key={index}
+                      className="text-xs lg:text-sm xl:text-base | border p-2 bg-blue-800 text-white border-black whitespace-nowrap text-center"
+                    >
+                      {column}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {Placeholders.map((placeholder, index) => {
                 return (
-                  <th
-                    key={index}
-                    className="text-xs lg:text-sm xl:text-base | border p-2 bg-blue-800 text-white border-black whitespace-nowrap text-center"
-                  >
-                    {column}
-                  </th>
+                  <tr key={index}>
+                    {COLUMNS.map((column, index) => {
+                      return (
+                        <td
+                          key={index}
+                          className="text-xs lg:text-sm xl:text-base | border p-2 whitespace-nowrap text-center"
+                        >
+                          {placeholder[column as keyof typeof placeholder]}
+                        </td>
+                      );
+                    })}
+                  </tr>
                 );
               })}
-            </tr>
-          </thead>
-          <tbody>
-            {Placeholders.map((placeholder, index) => {
-              return (
-                <tr key={index}>
-                  {COLUMNS.map((column, index) => {
-                    return (
-                      <td
-                        key={index}
-                        className="text-xs lg:text-sm xl:text-base | border p-2 whitespace-nowrap text-center"
-                      >
-                        {placeholder[column as keyof typeof placeholder]}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        ""
+      )}
     </Primitive>
   );
 };
