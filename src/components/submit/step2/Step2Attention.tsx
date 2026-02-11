932bc14f5b820d4309127943e5f5bfbc90214c2f
agw2005 <danial.agw2005@gmail.com>
2026-02-11 10:26:21 +0700

    refactor: removed unused files

--- src/components/submit/step2/Step2Attention.tsx
+++
@@ -1,9 +0,0 @@
-const Step2Attention = () => {
-  return (
-    <div className="bg-blue-50 text-blue-700 p-3 mb-6 text-sm rounded-md">
-      <b>INFO!</b> Your PR Number is in No. PR field, and No. Form is for tracking your form
-    </div>
-  );
-};
-
-export default Step2Attention;
