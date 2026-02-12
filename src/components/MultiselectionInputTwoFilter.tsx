c006018437525c720a9044adbd2b1d256a4ecccf
agw2005 <danial.agw2005@gmail.com>
2026-02-12 12:36:29 +0700

    feat(RemovableSelections.tsx, MultiselectionInputTwoFilter.tsx, FourthStep.tsx): added generic multiple selection input with two selection component to replace similar input elements

---
+++ src/components/MultiselectionInputTwoFilter.tsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import RemovableSelections from "./RemovableSelections";
+
+interface TwoFilterStringToStringMapping {
+  section: string;
+  members: string[];
+}
+
+interface MultiselectionInputTwoFilterProps {
+  label: string;
+  defaultFilterName: string;
+  defaultFilterId: string;
+  revealedFilterName: string;
+  revealedFilterId: string;
+  requiredInput: boolean;
+  color: string;
+  colorIntensity: string;
+  defaultFilterDefaultValue: string;
+  revealedFilterDefaultValue: string;
+  mappings: TwoFilterStringToStringMapping[];
+}
+
+const MultiselectionInputTwoFilter = ({
+  label,
+  defaultFilterName,
+  defaultFilterId,
+  revealedFilterName,
+  revealedFilterId,
+  requiredInput,
+  color,
+  colorIntensity,
+  defaultFilterDefaultValue,
+  revealedFilterDefaultValue,
+  mappings,
+}: MultiselectionInputTwoFilterProps) => {
+  const [selections, setSelections] = useState<string[]>([]);
+  const [defaultFilter, setDefaultFilter] = useState<string | undefined>(
+    undefined,
+  );
+  const [revealedFilter, setRevealedFilter] = useState<string | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    console.log(selections);
+  }, [selections]);
+
+  return (
+    <div className="flex flex-col">
+      <div className="h-8 lg:h-9 xl:h-10 | flex">
+        <div
+          className={`text-xs lg:text-sm xl:text-base | font-bold rounded-tl-xl h-full justify-self-center border flex items-center px-2 border-r-0 border-${color}-${colorIntensity} bg-${color}-${colorIntensity} text-white select-none`}
+        >
+          {label}
+          {requiredInput ? "*" : ""}
+        </div>
+        <select
+          name={defaultFilterName}
+          id={defaultFilterId}
+          className={`text-xs lg:text-sm xl:text-base | w-full h-full px-4 border border-${color}-${colorIntensity} text-${color}-${colorIntensity} bg-white/50 outline-none flex-1`}
+          onChange={(e) => {
+            if (revealedFilter !== undefined) {
+              setRevealedFilter("");
+            }
+            const newDefaultFilter = e.target.value;
+            setDefaultFilter(newDefaultFilter);
+          }}
+          value={defaultFilter}
+        >
+          <option value="" disabled selected>
+            {defaultFilterDefaultValue}
+          </option>
+          {mappings.map((mapping, index) => {
+            return (
+              <option key={index} value={mapping.section}>
+                {mapping.section}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          name={revealedFilterName}
+          id={revealedFilterId}
+          className={`text-xs lg:text-sm xl:text-base | w-full rounded-tr-xl h-full px-4 border border-${color}-${colorIntensity} text-${color}-${colorIntensity} bg-white/50 outline-none flex-1`}
+          onChange={(e) => {
+            const newRevealedFilter = e.target.value;
+            setRevealedFilter("");
+            setSelections([...selections, newRevealedFilter]);
+          }}
+          value={revealedFilter}
+        >
+          <option value="" disabled selected>
+            {revealedFilterDefaultValue}
+          </option>
+          {mappings
+            .find((mapping) => mapping.section === defaultFilter)
+            ?.members.map((member, index) => {
+              return (
+                <option key={index} value={member}>
+                  {member}
+                </option>
+              );
+            })}
+        </select>
+      </div>
+      <RemovableSelections
+        color="green"
+        colorIntensity="600"
+        array={selections}
+        arraySetter={setSelections}
+      />
+    </div>
+  );
+};
+
+export default MultiselectionInputTwoFilter;
---
+++ src/components/RemovableSelections.tsx
@@ -0,0 +1,42 @@
+interface RemovableSelections {
+  color: string;
+  colorIntensity: string;
+  array: string[];
+  arraySetter: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const RemovableSelections = ({
+  color,
+  colorIntensity,
+  array,
+  arraySetter,
+}: RemovableSelections) => {
+  return (
+    <div
+      className={`text-xs lg:text-sm xl:text-base | w-full flex flex-wrap gap-2 min-h-10 font-bold rounded-b-xl justify-self-center border border-${color}-${colorIntensity} text-white select-none p-2`}
+    >
+      {array.map((selection, index) => {
+        return (
+          <div className="flex" key={index}>
+            <div
+              className={`text-xs lg:text-sm | font-normal bg-${color}-900 rounded-l-xl p-2 flex items-center | to-handle-long-texts:( min-w-0 truncate max-w-64 )`}
+            >
+              {selection}
+            </div>
+            <div
+              className="text-xs lg:text-sm | bg-red-900 hover:bg-red-900/85 active:bg-red-900/70 | font-normal rounded-r-xl p-2 text-white"
+              onClick={() => {
+                const newSelections = array.filter((_, idx) => idx !== index);
+                arraySetter(newSelections);
+              }}
+            >
+              X
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RemovableSelections;
