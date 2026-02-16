4d7aa47bf48c216d7c458b0db4faed476e6b43c9
agw2005 <danial.agw2005@gmail.com>
2026-02-16 09:11:25 +0700

    feat(Primitive.tsx): simplified nav bar

--- src/components/Primitive.tsx
+++ src/components/Primitive.tsx
@@ -5,55 +5,51 @@ interface PrimitiveProps {
   children: React.ReactNode;
 }
 
-const NAVIGATIONS = [
-  { name: "Webform", link: "/" },
-  { name: "Home", link: "/" },
-  { name: "Submit Form", link: "/submit" },
-  { name: "Approval Menu", link: "/login" },
-  { name: "Budget", link: "/budget" },
-  { name: "Account", link: "/login" },
-];
-
+const LINKS = {
+  home: "/",
+  submit: "/submit",
+  approval: "/login", //is /login for now. in the future, direct to approval menu page directly
+  budget: "/budget",
+  login: "/login",
+};
 const Primitive = ({ children }: PrimitiveProps) => {
   return (
     <div className="bg-yellow-600/25 min-h-screen pb-16">
       <nav className="pl-2 lg:pl-4 xl:pl-6 2xl:pl-8 | flex justify-between gap-2 bg-black text-white sticky inset-0 z-100">
         <div className="gap-4 lg:gap-6 xl:gap-8 2xl:gap-10 | my-1 lg:my-2 xl:my-3 2xl:my-4 | flex flex-wrap items-center text-center ">
-          {NAVIGATIONS.map((navigation, index) => {
-            if (navigation.name === "Webform") {
-              return (
-                <Link
-                  to={navigation.link}
-                  key={index}
-                  className="text-xs lg:text-base xl:text-2xl | hover:bg-linear-to-r hover:from-[#25ab81ff] hover:via-[#3c8ecbff] hover:to-[#0d2f78ff] hover:text-transparent hover:bg-clip-text transition | font-bold bg-linear-to-r from-[#0d2f78ff] via-[#3c8ecbff] to-[#25ab81ff] text-transparent bg-clip-text"
-                  title="Created by :&#10;Danial Al-Ghazali Walangadi (UNNES 2304130143)&#10;and Antonio Vianzar (UNNES 2304130173)"
-                >
-                  WEBFORM
-                </Link>
-              );
-            } else
-              return (
-                <Link
-                  to={navigation.link}
-                  key={index}
-                  className="text-xs lg:text-base | hover:text-yellow-300 transition"
-                >
-                  {navigation.name}
-                </Link>
-              );
-          })}
+          <Link
+            to={LINKS.home}
+            className="text-xs lg:text-base xl:text-2xl | hover:bg-linear-to-r hover:from-[#25ab81ff] hover:via-[#3c8ecbff] hover:to-[#0d2f78ff] hover:text-transparent hover:bg-clip-text transition | font-bold bg-linear-to-r from-[#0d2f78ff] via-[#3c8ecbff] to-[#25ab81ff] text-transparent bg-clip-text"
+            title="Created by :&#10;Danial Al-Ghazali Walangadi (UNNES 2304130143)&#10;and Antonio Vianzar (UNNES 2304130173)"
+          >
+            WEBFORM
+          </Link>
+          <Link
+            to={LINKS.submit}
+            className="text-xs lg:text-base | hover:text-yellow-300 transition"
+          >
+            Submit Form
+          </Link>
+          <Link
+            to={LINKS.approval}
+            className="text-xs lg:text-base | hover:text-yellow-300 transition"
+          >
+            Approval Menu
+          </Link>
+          <Link
+            to={LINKS.budget}
+            className="text-xs lg:text-base | hover:text-yellow-300 transition"
+          >
+            Budget
+          </Link>
         </div>
         <div className="flex flex-wrap">
           <ForexInformation />
           <Link
-            to={
-              NAVIGATIONS.filter(
-                (navigation) => navigation.name === "Account",
-              )[0].link
-            }
+            to={LINKS.login}
             className="text-xs lg:text-base | px-2 lg:px-4 xl:px-6 2xl:px-8 | bg-black hover:bg-white hover:text-black active:bg-gray-800 active:text-white | flex items-center"
           >
-            <p className="select-none">User</p>
+            <p className="select-none">Requestor</p>
           </Link>
         </div>
       </nav>
