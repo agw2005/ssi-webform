e222d88137452a4ebc67c4096a0ed1cad5050d67
agw2005 <danial.agw2005@gmail.com>
2026-06-02 14:14:22 +0700

    feat(defaultSupervisor.ts, .env.example): hoist default supervisors settings to the env file

--- client/src/helper/defaultSupervisor.ts
+++ client/src/helper/defaultSupervisor.ts
@@ -1,7 +1,11 @@
 import type { SupervisorNRPs } from "@scope/server";
 
-export const defaultSupervisorsNRP: SupervisorNRPs = {
-  approver: ["002162R"],
-  releaser: ["002162R", "000032R", "000066R"],
-  administrator: ["000308"],
-};
+const rawSupervisors: string = import.meta.env.VITE_SUPERVISORS_NRP;
+
+export const defaultSupervisorsNRP: SupervisorNRPs = rawSupervisors
+  ? (JSON.parse(rawSupervisors) as SupervisorNRPs)
+  : {
+    approver: [],
+    releaser: [],
+    administrator: [],
+  };
